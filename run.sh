cfb3f82ebcc7aa87a624280d513c0c5381832bdc
wato <onionhop@gmail.com>
2026-06-03 08:13:27 -0400

    Add comments to run.sh for clarity
    Added comments explaining the need for local execution of the script.

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# we need this because pipe bash is really shitty so we gotta execute it locally in order for it to even run right
+# why?
+# fuck if i know bro.
+# that's what chatgpt told me.
 
 curl -fsSL https://raw.githubusercontent.com/watodoto/aio/refs/heads/main/script.sh -o /tmp/aio.sh && bash /tmp/aio.sh
